4b389f1522857afab22569c14d89ba74d89c952c
AdrianL769 <144189536+AdrianL769@users.noreply.github.com>
2025-02-26 12:49:55 -0500

    Add files via upload

--- Anova-test.r
+++ Anova-test.r
@@ -6,3 +6,8 @@ PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poiso
 #Treament types are effective against all poison types/there is a significance between treatment and poison types
 #Read the dataset in a variable
 df <-read.csv(PATH)
+#Understand levels/classes in datasets
+levels(factor(df$treat))levels(factor(df$treat))
+#Optional: Plot data
+ggplot(df, aes(x = poison, y = time, fill = poison)) + geom_boxplot() + geom_jitter(shape = 15, color = "steelblue", position = position_jitter(0.21)) + theme_classic()
+
